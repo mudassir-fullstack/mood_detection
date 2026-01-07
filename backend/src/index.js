8663eaecd92701e08af259a8e271b343908216ab
muhammad-hassan-codes <muhammadhassan.dev24@gmail.com>
2026-01-07 20:08:29 +0500

    hoja

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+import prisma from './prismaClient.js';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -24,6 +25,17 @@ app.use('/api', contactRoutes);
 app.get('/', (req, res) => {
   res.json({ message: "Mood Genius API Running!" });
 });
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json({ success: true, users });
+  } catch (err) {
+    console.error("DB connection error:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
